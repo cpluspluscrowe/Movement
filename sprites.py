d0e26ea792b981a7a8447d71dd74853b2dabcf23
Crowe <ccrowe@hdrinc.com>
2016-08-04 13:59:29 -0500

    Added player classes and assigned them to objects.  this is 3 v 1 right now.

--- sprites.py
+++ sprites.py
@@ -6,7 +6,26 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
-
+class Match():
+    def __init__(self,team1,team2):
+        self.Team1 = team1
+        self.Team2 = team2
+class Team():
+    def __init__(self,players,color):
+        self.Players = players
+        self.Color = color
+class Ball():
+    def __init__(self,image):
+        self.Image = image
+class Player():
+    def __init__(self,image,team):
+        self.Team = team
+        self.Image = image
+        self.HasBall = False
+    def Pass(self,angle,distance):
+        pass
+    def Run(self,angle,distance):
+        pass
 class Block(pygame.sprite.Sprite):
     """
     This class represents the ball
@@ -39,51 +58,45 @@ class Block(pygame.sprite.Sprite):
     def update(self):
         """ Called each frame. """
  
-        # Move block down one pixel
-        self.rect.center = (self.rect.center[0]+random.randrange(-3,3),self.rect.center[1]+random.randrange(-3,3))
+        # Don't move for now
+        #self.rect.center = (self.rect.center[0]+random.randrange(-3,3),self.rect.center[1]+random.randrange(-3,3))
  
-class Player(Block):
-    """ The player class derives from Block, but overrides the 'update'
-    functionality with new a movement function that will move the block
-    with the mouse. """
-    def update(self):
-        pass
  
 # Initialize Pygame
 pygame.init()
  
 # Set the height and width of the screen
-screen_width = 700
-screen_height = 400
+screen_width = 600
+screen_height = 600
 screen = pygame.display.set_mode([screen_width, screen_height])
  
 # This is a list of 'sprites.' Each block in the program is
 # added to this list. The list is managed by a class called 'Group.'
-block_list = pygame.sprite.Group()
+#block_list = pygame.sprite.Group()
  
 # This is a list of every sprite. All blocks and the player block as well.
 all_sprites_list = pygame.sprite.Group()
- 
+positions = [[20,20],[270,540],[540,0]]
 for i in range(3):
     # This represents a block
-    block = Block(BLACK)
- 
+    player = Player(Block(BLACK),"Blue")
+    print(positions[i])
     # Set a random location for the block
-    block.rect.x = random.randrange(screen_width)
-    block.rect.y = random.randrange(screen_height)
+    player.Image.rect.x = positions[i][0]
+    player.Image.rect.y = positions[i][1]
  
     # Add the block to the list of objects
-    block_list.add(block)
-    all_sprites_list.add(block)
+    #block_list.add(block)
+    all_sprites_list.add(player.Image)
  
 # Create a red player block
-player = Player(RED)
-all_sprites_list.add(player)
+player = Player(Block(RED),"Red")
+player.Image.rect.x = 270
+player.Image.rect.y = 270
+all_sprites_list.add(player.Image)
  
-# Loop until the user clicks the close button.
 done = False
  
-# Used to manage how fast the screen updates
 clock = pygame.time.Clock()
  
 score = 0
@@ -101,15 +114,12 @@ while not done:
     all_sprites_list.update()
  
     # See if the player block has collided with anything.
-    blocks_hit_list = pygame.sprite.spritecollide(player, block_list, False)
+    #blocks_hit_list = pygame.sprite.spritecollide(player, block_list, False)
  
     # Check the list of collisions.
-    for block in blocks_hit_list:
-        score += 1
-        print(score)
  
         # Reset block to the top of the screen to fall again.
-        block.reset_pos()
+    player.Image.reset_pos()
  
     # Draw all the spites
     all_sprites_list.draw(screen)
